7f917fcfac6113966738ae913be6dd89e55be3af
Jonathan_Symons7 <jsym922@aucklanduni.ac.nz>
2019-09-15 19:48:56 +1200

    Include basic troubleshooting \n\n Checks the answers that people input to make sure there are no duplicates or empty answers. If there are, then we don't write to the database and get them to resolve the issue. Means we have less data cleaning to do at the other end.

--- public/app.js
+++ public/app.js
@@ -30,25 +30,61 @@
     const saveButton = document.querySelector("#saveButton")    
 
     saveButton.addEventListener("click", function() {
-        const textToSave = inputTextField.value;
-        console.log("I m going to save " + option1.value + " to Firestore");
-        docRef.add({
-            name: inputTextField.value,
-            preference1: option1.value,
-            preference2: option2.value,
-            preference3: option3.value,
-            preference4: option4.value,
-            preference5: option5.value,
-            preference6: option6.value,
-            preference7: option7.value,
-            preference8: option8.value,
-            alreadyAssigned: firstAssignment.value,
-            currentLT: notAssignable.value,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        }).then(function() {
-            console.log("Status saved!");
-        }).catch(function(error) {
-            console.log("Got an error: ", error);
-        });
+        var fieldEntries = [ option1.value,
+                            option2.value,
+                            option3.value,
+                            option4.value,
+                            option5.value,
+                            option6.value,
+                            option7.value,
+                            option8.value,
+                            firstAssignment.value,
+                            notAssignable.value];
+        console.log(emptyEntries(fieldEntries))
+        if(emptyEntries(fieldEntries)) {
+            document.querySelector("#Status").innerHTML = "Please fill in all fields"
+            console.log("Duplicate entries")
+        } else if (hasDuplicates(fieldEntries)) {
+            document.querySelector("#Status").innerHTML = "Duplicate Entries"
+            console.log("Not all entries filled in")
+        } else {
+            console.log("I m going to save " + option1.value + " to Firestore");
+            docRef.add({
+                name: inputTextField.value,
+                preference1: option1.value,
+                preference2: option2.value,
+                preference3: option3.value,
+                preference4: option4.value,
+                preference5: option5.value,
+                preference6: option6.value,
+                preference7: option7.value,
+                preference8: option8.value,
+                alreadyAssigned: firstAssignment.value,
+                currentLT: notAssignable.value,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            }).then(function() {
+                document.querySelector('#Status').innerHTML = "Successfully saved results";
+                console.log("Status saved!");
+            }).catch(function(error) {
+                document.querySelector("#status").innerHTML = "Was not able to save"
+                console.log("Got an error: ", error);
+            });
+        }
     })
+
+    
+    function hasDuplicates(array) {
+        return (new Set(array)).size !== array.length;
+    }
+
+
+    function emptyEntries(array){
+        for(var i=0; i<array.length; i++) {
+            if (array[i] == "") {
+                console.log("Empty")
+                return true;
+            }
+        }
+        return false; 
+    }
 //})
