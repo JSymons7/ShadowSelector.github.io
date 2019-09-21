86c823c55fcc65d5394f86eb362da55eeddd5de7
jonathan_symons <jonathan.symons@fonterra.com>
2019-09-22 10:26:08 +1200

    Load data and solve
    Updates the data loading to handle the JSON format taken from the NoSQL database. Still need to update the code so it pulls straight from the database. The solver now has the two stages. Firstly ensuring that the worst person is as best off as possible. Then, without anyone being worse than that, can we make the group better off. It also has the ability to ensure that they cannot be assigned to their L3 if they don't want to. Also, makes it so that they aren't assigned to the L3 they were assigned to for the first quarter. This will need to be changed if they use this code again next year.

--- src/load_data.jl
+++ src/load_data.jl
@@ -1,11 +1,76 @@
-using CSV
+using CSV, HTTP, JSON
+
+
+mutable struct Preference
+    name                            ::String
+    preferences                     ::Dict{String, Int}
+    currentLT                       ::String
+    alreadyAssigned                 ::String
+    enableAssignmentToCurrentLT     ::Bool
+end
+
+Preference(name) = Preference(
+    name,
+    Dict{String, Int}(),
+    "",
+    "",
+    0
+)
 
 
 function load_data(args)
     env = Dict{String, Any}()
 
-    data = CSV.read("..\\data\\data.csv")
+    data = JSON.parse(open("..\\data\\preferences.json", "r"))
+    # data = HTTP.get("https://firestore.googleapis.com/v1/projects/shadow-5f1c5123/databases/(default)/documents/preferences")
+    # println(data)
+
+    MC = Dict{String, Any}()
+    for fields in data["documents"]
+        name = fields["fields"]["name"]["stringValue"]
+        MC[name] = Preference(name)
+
+        MC[name].preferences[fields["fields"]["preference1"]["stringValue"]] = 1
+        MC[name].preferences[fields["fields"]["preference2"]["stringValue"]] = 2
+        MC[name].preferences[fields["fields"]["preference3"]["stringValue"]] = 3
+        MC[name].preferences[fields["fields"]["preference4"]["stringValue"]] = 4
+        MC[name].preferences[fields["fields"]["preference5"]["stringValue"]] = 5
+        MC[name].preferences[fields["fields"]["preference6"]["stringValue"]] = 6
+        MC[name].preferences[fields["fields"]["preference7"]["stringValue"]] = 7
+        MC[name].preferences[fields["fields"]["preference8"]["stringValue"]] = 8
+        MC[name].preferences[fields["fields"]["preference9"]["stringValue"]] = 9
+        MC[name].preferences[fields["fields"]["preference10"]["stringValue"]] = 10
+        MC[name].preferences[fields["fields"]["preference11"]["stringValue"]] = 11
+        MC[name].preferences[fields["fields"]["preference12"]["stringValue"]] = 12
+        
+
+        MC[name].currentLT = fields["fields"]["currentLT"]["stringValue"]
+
+        MC[name].alreadyAssigned = fields["fields"]["alreadyAssigned"]["stringValue"]
+        MC[name].preferences[MC[name].alreadyAssigned] = 1000
+
+        MC[name].enableAssignmentToCurrentLT = fields["fields"]["enableAssignmentToCurrentLT"]["booleanValue"]
+    end
+
+    println(MC)
+
+    #data = HTTP.get("https://firestore.googleapis.com/v1/projects/shadow-5f1c5123/databases/(default)/documents/preferences")
     
-    env["data"] = data
+    env["data"] = MC
+
+    env["LT"] = [
+        "Ben Schurr",
+        "Brendan Ryan",
+        "Brent Mealings",
+        "Chris Greenough",
+        "Chris Rowe",
+        "Eric Hansen",
+        "Gordon Rettkowicz",
+        "Jo Martell",
+        "Mark Kennerley",
+        "Paul Campbell",
+        "Piers Shore",
+        "Priya Singh",
+        "Simon Till"]
     return env
 end
--- src/solve.jl
+++ src/solve.jl
@@ -1,41 +1,112 @@
-using JuMP, CSV
-import Cbc
+using JuMP, CSV, DataFrames
+import GLPK
 
 
 function solve(env)
     data = env["data"]
 
-    println(env)
+    LT = env["LT"]
 
-    LT = names(data)[2:end]
+    MC = keys(env["data"])
 
-    MC = data[:MC]
-
-    m = Model(with_optimizer(Cbc.Optimizer))
+    m = Model(with_optimizer(GLPK.Optimizer, tm_lim = 60000))
 
     @variables m begin
         assignment[ lt = LT, mc = [m for m in MC], periods = 1:3] >= 0, Bin
+        z
     end
 
-    @objective(m, Max, sum(assignment))
-    for t in 1:3
+    T = 1:3
+
+    for t in T
         for mc in MC  
             @constraint(m, sum(assignment[:, mc, t]) == 1)
         end
 
         for lt in LT
-            @constraint(m, sum(assignment[lt, :, t]) == 1)
+            @constraint(m, sum(assignment[lt, :, t]) <= 1)
         end
     end
 
-    optimize!(m)
-    f = open("FileName.csv", "w")
-    for t in 1:3
-        println(value.(assignment[:,:,t]))
-
-        CSV.write(f,  value.(assignment[:,:,t]))
+    for lt in LT
+        for mc in MC
+            @constraint(m, sum(assignment[lt, mc, :]) <= 1)
+        end
     end
+
+    for mc in MC
+        @constraint(m, sum(assignment[data[mc].alreadyAssigned, mc, :]) ==0)
+    end
+
+    for mc in MC
+        if data[mc].enableAssignmentToCurrentLT != true
+            @constraint(m, sum(assignment[data[mc].currentLT, mc, :]) == 0)
+        end
+    end
+
+    for mc in MC
+        @constraint(m, sum(data[mc].preferences[lt] * assignment[lt, mc, t] for lt in LT, t in T) <= z)
+    end
+
+    #Make worst person as good as possible
+    @objective(m, Min, z)
+
+    optimize!(m)
+    println(value(z))
+
+    @constraint(m, z <= objective_value(m))
+
+    #Add constraint so they don't get any worse
+
+    #Make everyone as good as possible
+    @objective(m, Min, sum(data[mc].preferences[lt] * assignment[lt, mc, t] for lt in LT, mc in MC, t in T))
+    optimize!(m)
+
+    f = open("FileName.csv", "w")
+
+    results = Dict{String, Any}()
+
+
+
+    # output = DataFrame(index = [mc for mc in MC], columns = [t for t in T])
+    # println(output)
+    columns = [Symbol(col) => String[] for col in T]
+    df = DataFrame(fill(String, length(MC)), Symbol.(MC), length(T))
+    df[2, Symbol.("Jonathan Symons")] = "5"
+    println("HERE")
+    println(df)
+    for mc in MC
+        println(mc)
+        # println(sum(data[mc].preferences[lt] * value(assignment[lt, mc, t]) for lt in LT, t in T))
+        results[mc] = Dict{Int, String}()
+        for lt in LT   
+
+            for t in T
+                if value.(assignment[lt, mc, t]) == 1
+                    println(assignment[lt, mc, t])
+                    # results[mc][t] = lt
+                    # output[mc,t] = lt
+                    df[t, Symbol.(mc)] = lt
+                end
+            end
+        end
+        # push!(df, results[mc])
+        # println(results[mc])
+        # df
+    end
+    println(df)
+    # itr = (merge((; key = key), val) for (key, val) in results)
+    # itr |> CSV.write("test.csv")
+
+
+    println("Objective is ", objective_value(m))
+
+    # CSV.write(f, DataFrame(results))
+    CSV.write(f, df)
     close(f)
+
+    # println(output)
+    # output.to_csv(path_or_buf = "output.csv")
 
     return m
 end
